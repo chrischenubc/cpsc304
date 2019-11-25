b81190cc1191b4b2d15da5cd3990292ea394f075
Yu Chen <chrischenubc@gmail.com>
2019-11-25 00:00:42 -0800

    last minute update

--- SQL/clerkTransactions.sql
+++ SQL/clerkTransactions.sql
@@ -16,7 +16,7 @@ WHERE confNo = 000000001;
 -- View if the car type reserved is still available on this branch given a confirmation number (e.g 000000001)
 
 SELECT COUNT(*)
-FROM Reservations R, Vehicles V 
+FROM Reservations R, Vehicles V
 WHERE R.confNo = 000000001 AND  R.vtname = V.vtname AND V.location = '1250 Granville St'  AND V.status = 'available';
 
 -- Make a reservation prior to the rental(Every rental is required to pair with a reservation even though the customer just drop in and rent the car) 
@@ -40,7 +40,7 @@ Where vlicense = '896REN';
 
 -- Check if a car can be returned given a vlicense(if not then 0 is returned) 
 SELECT COUNT(*)
-FROM Vehicles 
+FROM Vehicles
 Where vlicense='896REN' AND status ='rented';
 
 -- Fetch the necessary details(not including the calculation) for the customer after returning a car
@@ -62,115 +62,114 @@ VALUES(000000001,TO_TIMESTAMP('2019-12-18 17:45', 'YYYY-MM-DD HH24:MI'),12332112
 
 -- Daily rental report for all branches
 
--- Generate daily report of all rental infomation for all branches given a specific date
+-- Generate daily report of all rental infomation for all branches given a specific timestamp of the day
 SELECT location, city, vtname
-FROM Vehicles V, Rentals R 
-WHERE trunc(R.fromDateTime,'YYYY-MM-DD') = to_date('2019-12-15', 'YYYY-MM-DD') AND V.vlicense = R.vlicense
+FROM Vehicles V, Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R.vlicense
 GROUP BY V.location,V.city,V.vtname;
 
--- Generate daily report for number of rentals per category across all branches given a specific date
-SELECT COUNT(*) vtname
-FROM Vehicles V, Rentals R 
-WHERE to_date(R.fromDateTime,'YYYY-MM-DD') = to_date('2019-12-15', 'YYYY-MM-DD') AND V.vlicense = R.vlicense
+-- Generate daily report for number of rentals per category across all branches given a specific timestamp of the day
+SELECT COUNT(*),V.vtname
+FROM Vehicles V, Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R.vlicense
 GROUP BY V.vtname;
 
--- Generate daily report for total number of rentals at each branch
-SELECT COUNT(*)
-FROM Vehicles V, Rentals R 
-WHERE trunc(R.fromDateTime) = to_date('2019-12-15', 'YYYY-MM-DD') AND V.vlicense = R.vlicense
+-- Generate daily report for total number of rentals at each branch given a specific timestamp of the day
+SELECT COUNT(*), V.location, V.city
+FROM Vehicles V, Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R.vlicense
 GROUP BY V.location,V.city;
 
--- Generate daily report for total number of rentals across the compnay
+-- Generate daily report for total number of rentals across the compnay given a specific timestamp of the day
 SELECT COUNT(*)
-FROM Rentals R 
-WHERE trunc(R.fromDateTime) = to_date('2019-12-15', 'YYYY-MM-DD') AND V.vlicense = R.vlicense;
+FROM Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI');
 
 -- Daily rental report specific to a branch
 
--- Generate daily report of all rental infomation for a specific branch given a specific date
-SELECT * 
-FROM Vehicles V, Rentals R 
-WHERE trunc(R.fromDateTime) = to_date('2019-12-15', 'YYYY-MM-DD') AND V.location = '1250 Granville St' AND V.city='Vancouver' AND V.vlicense = R.vlicense;
+-- Generate daily report of all rental infomation for a specific branch given a specific timestamp of the day
+SELECT *
+FROM Vehicles V, Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.location = '10376 King George Blvd' AND V.city='Surrey' AND V.vlicense = R.vlicense;
 GROUP BY V.vtname;
 
--- Generate daily report for number of rentals per category across all branches given a specific date
+-- Generate daily report for number of rentals per category across all branches given a specific timestamp of the day
 SELECT COUNT(*)
-FROM Vehicles V, Rentals R 
-WHERE trunc(R.fromDateTime) = to_date('2019-12-15', 'YYYY-MM-DD') AND V.location = '1250 Granville St' AND V.city='Vancouver' AND V.vlicense = R.vlicense;
+FROM Vehicles V, Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.location = '10376 King George Blvd' AND V.city='Surrey' AND V.vlicense = R.vlicense;
 GROUP BY V.vtname;
 
--- Generate daily report for total number of rentals at a specific branch
+-- Generate daily report for total number of rentals at a specific branch given a specific timestamp of the day
 SELECT COUNT(*)
-FROM Vehicles V, Rentals R 
-WHERE trunc(R.fromDateTime) = to_date('2019-12-15', 'YYYY-MM-DD') AND V.location = '1250 Granville St' AND V.city='Vancouver' AND V.vlicense = R.vlicense；
+FROM Vehicles V, Rentals R
+WHERE trunc(R.fromDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.location = '10376 King George Blvd' AND V.city='Surrey' AND V.vlicense = R.vlicense;
 
 
 
 -- Daily return report for all branches
 
--- Generate daily report of all return infomation for all branches given a specific date
-SELECT * 
+-- Generate daily report of all return infomation for all branches given a specific date given a specific timestamp of the day
+SELECT V.location,V.city,V.vtname
 FROM Vehicles V, Returns R, Rentals R2
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
 GROUP BY V.location,V.city,V.vtname;
 
--- Generate daily report for number of renturns per category across all branches given a specific date
-SELECT COUNT(*)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
+-- Generate daily report for number of renturns per category across all branches given a specific given a specific timestamp of the day
+SELECT COUNT(*), V.vtname
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
 GROUP BY V.vtname;
 
--- Generate daily report for revenue per category of renturns aross all branches
+-- Generate daily report for revenue per category of renturns aross all branches given a specific timestamp of the day
 SELECT SUM(R.value)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
 GROUP BY V.vtname;
 
--- Generate daily report for number of vehicles return per branches
-SELECT COUNT(*)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
+-- Generate daily report for number of vehicles return per branches given a specific timestamp of the day
+SELECT COUNT(*),V.location, V.city
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
 GROUP BY V.location, V.city;
 
--- Generate daily report for subtotal revenue per branches
-SELECT SUM(R.value)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
+-- Generate daily report for subtotal revenue per branches given a specific timestamp of the day
+SELECT SUM(R.value), V.location, V.city
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
 GROUP BY V.location, V.city;
 
--- Generate daily report for grand total revenue across the company
-SELECT COUNT(*)
+-- Generate daily report for grand total revenue across the company given a specific timestamp of the day
 SELECT SUM(R.value)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid;
 
 
 -- Daily return report for a specific branch
 
--- Generate daily report of all return infomation a specific branch given a specific date
-SELECT * 
+-- Generate daily report of all return infomation a specific branch given a specific date given a specific timestamp of the day
+SELECT *
 FROM Vehicles V, Returns R, Rentals R2
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver'
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver'
 GROUP BY V.vtname;
 
--- Generate daily report for number of renturns per category at a specific branch given a specific date
+-- Generate daily report for number of renturns per category at a specific branch given a specific date given a specific timestamp of the day
+SELECT COUNT(*) ,V.vtname
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver'
+GROUP BY V.vtname;
+
+-- Generate daily report for revenue per category of at a specifc branch given a specific timestamp of the day
+SELECT SUM(R.value), V.vtname
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver'
+GROUP BY V.vtname;
+
+-- Generate daily report for number of vehicles return at a specific branch given a specific timestamp of the day
 SELECT COUNT(*)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver'
-GROUP BY V.vtname;
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver';
 
--- Generate daily report for revenue per category of at a specifc branch
+-- Generate daily report for the revenue at a specific branch given a specific timestamp of the day
 SELECT SUM(R.value)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver'
-GROUP BY V.vtname;
-
--- Generate daily report for number of vehicles return at a specific branch
-SELECT COUNT(*)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver';
-
--- Generate daily report for the revenue at a specific branch
-SELECT SUM(R.value)
-FROM Vehicles V, Returns R, Rentals R2 
-WHERE trunc(R.returnDateTime) = to_date('2019-12-18', 'YYYY-MM-DD') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver';
+FROM Vehicles V, Returns R, Rentals R2
+WHERE trunc(R.returnDateTime) = TO_TIMESTAMP('2019-11-15 00:00', 'YYYY-MM-DD HH24:MI') AND V.vlicense = R2.vlicense AND R.rid = R2.rid AND V.location ='burrad 2020' AND V.city ='Vancouver';
